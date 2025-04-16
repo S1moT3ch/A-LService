a4786cf3adda89d14b6e44dd022ad7e60970a6e0
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-04-16 17:22:24 +0200

    ok

---
+++ app/config/passport-config.js
@@ -0,0 +1,70 @@
+const passport = require ('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { emit } = require('process');
+const uri = "mongodb+srv://Simone:S4ikJ4B2oYjj6Qpt@cluster0.ungo5pt.mongodb.net/?appName=Cluster0";
+var userdb = 0;
+var userinput = 0;
+var userpassword = 0;
+var utenteTrovato = false;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("ACSAHomeConnect").command({ ping: 1 });
+      console.log("Db MongoDB connesso!");
+    } finally {
+    }
+  }
+run().catch(console.dir);
+
+
+passport.use(
+    'local-login',
+    new LocalStrategy((username, password, done) => {
+        userinput = username;
+        userpassword = password;
+        search().catch(console.dir);
+        async function search() {
+          try {
+            const database = client.db("ACSAHomeConnect");
+            userdb = database.collection("user");
+            const userc = await userdb.findOne({"username": userinput, "password": userpassword}, {_id: false, username: true, password: true});
+            if(userc){
+              utenteTrovato = true;
+              console.log('dbFind '+utenteTrovato);
+              console.log(userc);
+            }
+            if(utenteTrovato){
+              utenteTrovato = false;
+              const user = { id: 1, username: 'Simone'};
+              return done(null, user);
+            }
+            return done(null, false);
+          } 
+          finally {
+          }
+        }
+    })
+);
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    // Recupero untente in db
+    const user = { id: 1, username: 'Simone'};
+    done(null, user);
+});
+
+module.exports = passport;
