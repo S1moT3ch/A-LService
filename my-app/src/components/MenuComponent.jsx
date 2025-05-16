8d818dc924ac105b66335ab7e9c8a743acde6b49
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-05-16 20:39:25 +0200

    Revert "Menu_lista_14"
    This reverts commit 1c26abe286b0bc6d11e9726c43a18e6e9da61e3c.

--- my-app/src/components/MenuComponent.jsx
+++ my-app/src/components/MenuComponent.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import React from 'react';
-import './style/MenuComponent.css';
+import './components/style/MenuComponent.css';
 
 const MenuComponent = () => {
   const navigate = useNavigate();
